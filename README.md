7cd1a184c9b7f9ee45aa36f6833ca72a3e74389d
Ethan Moss <31551367+ethanmoss@users.noreply.github.com>
2022-06-22 22:21:41 -0500

    mvp initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # LivePerson-technical-exercise
- 
+
+## Setup:
+* Add in .env file and fill in the following fields with your own application credentials:
+    * SITE_ID
+* Install Dependencies:  
+`npm install`
+* Create LivePerson Conversation:  
+`node index.js`
+* Conversation will now be created and displayed on the [LiveEngage Admin Console](https://authentication.liveperson.net/)
---
+++ create_conversation.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const axios = require('axios');
+const WebSocket = require('ws');
+
+// Create promise to return JWT token
+const getToken = () => {
+    const url = `https://va.idp.liveperson.net/api/account/${process.env.SITE_ID}/signup`;
+    return axios.post(url);
+}
+
+// Open websocket using JWT Token and generate LivePerson conversation
+const openWebSocket = tokenResponse => {
+    const jwtToken = tokenResponse.data.jwt;
+    const url = `wss://va.msg.liveperson.net/ws_api/account/${process.env.SITE_ID}/messaging/consumer?v=3`;
+
+    // Create new websocket connection using LivePerson URL and JWT token
+    const ws = new WebSocket(url, {
+        headers: { Authorization: `jwt ${jwtToken}` }
+    });
+
+    // On websocket open, generate liveperson conversation
+    ws.on('open', () => {
+        console.log('Websocket successfully opened');
+        ws.send('{"kind":"req","id":1,"type":"cm.ConsumerRequestConversation"}');
+    });
+
+    // When a LivePerson conversation is generated, send a message that reads: "My first message"
+    ws.on('message', data => {
+        const response = JSON.parse(data);
+        const conversationId = response.body.conversationId;
+
+        ws.send(`{"kind":"req","id":2,"type":"ms.PublishEvent","body":{"dialogId":"${conversationId}","event":{"type":"ContentEvent","contentType":"text/plain","message":"My first message"}}}`);
+    });
+
+}
+
+// Resolve promises and generate conversation
+getToken()
+.then(jwt => openWebSocket(jwt))
+.then();
